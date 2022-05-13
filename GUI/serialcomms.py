70f044aa14209e34355aa39189ccb3435ffea8b7
Woraphon Choaywong <phoori12@gmail.com>
2022-05-13 21:48:11 +0200

    buffer fix 2

--- GUI/serialcomms.py
+++ GUI/serialcomms.py
@@ -10,7 +10,7 @@ class SerialComms:
         self.ser = serial.Serial()
         self.ser.port = ''
         self.ser.baudrate = baudrate
-        self.buffer = [None]*104
+        self.buffer = [None]*52
         self.cmdIndex = 0
         self.cmdLength = 52 * 2
         
@@ -56,7 +56,7 @@ class SerialComms:
             if (self.buffer[self.cmdIndex - 51] == b'#') and (self.buffer[self.cmdIndex - 50] == b's') and (self.buffer[self.cmdIndex - 1] == b'\r') and (self.buffer[self.cmdIndex] == b'\n'):
                 self.cmdIndex = 0
                 return_buffer = self.buffer
-                self.buffer = [None]*104
+                self.buffer = [None]*52
                 return return_buffer
             else:
                 if self.cmdIndex < self.cmdLength:
@@ -65,7 +65,8 @@ class SerialComms:
                     self.cmdIndex = 0
 
             if time.time() - loop_start_time > timeout:
-                self.buffer = [None]*104
+                self.buffer = [None]*52
+                self.cmdIndex = 0
                 return 0
         return 0
     
